dbc254891b138f9b51b7a7a5c9d93f7f3b2a4da9
Duncan McAllister <dmcallis@akamai.com>
2017-11-27 12:44:41 -0500

    Updared BQ client

--- bigquery-client/bigquery-client.py
+++ bigquery-client/bigquery-client.py
@@ -13,21 +13,21 @@ SELECT
   NTH(50, quantiles(onLoad))   ONLOAD_med,
   NTH(50, quantiles(visualComplete))   VISUAL_COMPLETE_med,
   NTH(50, quantiles(SpeedIndex))   SPEED_INDEX_med,
-  NTH(50, quantiles(bytesTotal))   TOTAL_med,
   NTH(50, quantiles(reqTotal))   TOTAL_reqs_med,
-  NTH(50, quantiles(bytesHtml))    HTML_med,
-  NTH(50, quantiles(reqHtml))    HTML_reqs_med,
-  NTH(50, quantiles(bytesJS))      JS_med,
-  NTH(50, quantiles(reqJS))      JS_reqs_med,
-  NTH(50, quantiles(bytesCSS))     CSS_med,
-  NTH(50, quantiles(reqCSS))     CSS_reqs_med,
-  NTH(50, quantiles(bytesImg))     IMG_med,
+  NTH(50, quantiles(bytesTotal))   TOTAL_med,
   NTH(50, quantiles(reqImg))     IMG_reqs_med,
-  NTH(50, quantiles(bytesFont))    FONT_med,
+  NTH(50, quantiles(bytesImg))     IMG_med,
+  NTH(50, quantiles(reqJS))      JS_reqs_med,
+  NTH(50, quantiles(bytesJS))      JS_med,
+  NTH(50, quantiles(reqCSS))     CSS_reqs_med,
+  NTH(50, quantiles(bytesCSS))     CSS_med,
+  NTH(50, quantiles(reqHtml))    HTML_reqs_med,
+  NTH(50, quantiles(bytesHtml))    HTML_med,
   NTH(50, quantiles(reqFont))    FONT_reqs_med,
+  NTH(50, quantiles(bytesFont))    FONT_med,
   NTH(50, quantiles(numDomains))    DOMAINS_med,
   NTH(50, quantiles(numDomElements))    DOM_ELEMS_med,
-FROM [httparchive:runs.2017_08_01_pages_mobile]
+FROM [httparchive:runs.2017_11_15_pages_mobile]
 WHERE rank <= 1000
 """)
 
@@ -39,4 +39,3 @@ rows = query.fetch_data()
 for row in rows:
     # Convert tuple to list and print
     print (row)
-    print (type(row))
